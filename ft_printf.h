c4f9be11cd43725880d277b582413b0b21f2b228
Zhunissali SHANABEK <zshanabe@e1r4p12.unit.ua>
2018-04-27 16:08:01 +0300

    [Add] - Saved specifier in struct

--- ft_printf.h
+++ ft_printf.h
@@ -6,19 +6,19 @@
 /*   By: zshanabe <zshanabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/26 21:48:23 by zshanabe          #+#    #+#             */
-/*   Updated: 2018/04/27 14:41:57 by zshanabe         ###   ########.fr       */
+/*   Updated: 2018/04/27 15:40:58 by zshanabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF
 # define FT_PRINTF
 # include "libft/libft.h"
-typedef struct	s_spec
+typedef struct	s_item
 {
 	int width;
 	int hyphen;
 	int precision;
 	char specifier;
-}				t_spec;
+}				t_item;
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zshanabe <zshanabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/25 14:17:14 by zshanabe          #+#    #+#             */
-/*   Updated: 2018/04/27 14:42:24 by zshanabe         ###   ########.fr       */
+/*   Updated: 2018/04/27 16:00:22 by zshanabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,47 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int ft_printf(const char * restrict format)
+int define_specifier(char c)
 {
-	ft_putstr(format);
+	int i;	
+	const char flags[] = "sSpdDioOuUxXcC";
+
+	i = 0;
+	while (flags[i])
+	{
+		if (flags[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void ft_printf(const char * restrict format)
+{
+	int i;
+	t_item form;
+
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			while(format[i])
+			{
+				if (define_specifier(format[i]))
+					form.specifier = format[i];
+				i++;
+			}
+		}
+		i++;
+	}
+	ft_putchar(form.specifier);
 }
 
 int main()
 {
-	ft_printf("ok%d");
+	int c;
+	char *s = "zhunissali";
+	c = printf("%s\n", s);
 }
