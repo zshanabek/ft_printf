c3fffb9103d3eabb6f5745950954ef4db41b714a
Zhunissali <zshanabek@gmail.com>
2018-05-22 21:59:11 +0300

    [Refactor] - (pf_flags) - norminette

--- pf_flags.c
+++ pf_flags.c
@@ -1,55 +1,55 @@
 #include "ft_printf.h"
 
-int find_minus(char *flags)
+int		find_minus(char *flags)
 {
 	char *minus;
 
 	minus = ft_strchr(flags, '-');
 	if (minus)
-		return (1);	
+		return (1);
 	return (0);
 }
 
-int find_zero(char *flags)
+int		find_zero(char *flags)
 {
 	int i;
 
 	i = 0;
-	while(flags[i])
+	while (flags[i])
 	{
-		if (ft_isdigit(flags[i-1]) == 0 && flags[i] == '0')
+		if (ft_isdigit(flags[i - 1]) == 0 && flags[i] == '0')
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-int find_plus(char *flags)
+int		find_plus(char *flags)
 {
 	char *plus;
 
 	plus = ft_strchr(flags, '+');
 	if (plus)
-		return (1);	
+		return (1);
 	return (0);
 }
 
-int find_space(char *flags)
+int		find_space(char *flags)
 {
 	char *space;
 
 	space = ft_strchr(flags, ' ');
 	if (space)
-		return (1);	
+		return (1);
 	return (0);
 }
 
-int find_hash(char *flags)
+int		find_hash(char *flags)
 {
 	char *hash;
 
 	hash = ft_strchr(flags, '#');
 	if (hash)
-		return (1);	
+		return (1);
 	return (0);
 }
